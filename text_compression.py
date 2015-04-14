e6c58d91eedd56b6d87e1ce219b57fd7ef0cba4e
PankajMoolrajani <pmool@nyu.edu>
2015-04-14 17:16:50 -0400

    summary
    desc

---
+++ text_compression.py
@@ -0,0 +1,18 @@
+"""
+Given a string you are provided with the possible operations.
+We can group the adjacent substrings, for e.g ABCABCBC can be compressed as
+2ABC1BC or 1ABCA2BC
+
+Among all the possible options, task is to find the resultant string with the minimum length.
+So for the above example the solution would be 2ABC1BC
+
+Another example would be
+FLFLAFLAFLAF
+Solution: 1FLF3LAF
+"""
+
+def main():
+    print "main"
+
+if __name__ == "__main__":
+    main()
